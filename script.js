d39449c65b3fe91468fb34647664b360f6fb96fd
2203417 <la2203417@uabc.edu.mx>
2026-01-08 11:18:54 -0800

    version final

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 const DEFAULT_COLOR = "#6d6969ff";
-const DEFAULT_MODE = "COLOR";
+const DEFAULT_MODE = "color";
 const DEFAULT_SIZE = 16;
 
 let currentColor = DEFAULT_COLOR;
 let currentMode = DEFAULT_MODE;
 let currentSize = DEFAULT_SIZE;
+
+function setCurrentColor(newColor){
+  currentColor= newColor;
+}
+
+function setCurrendMode(newMode){
+  currentMode = newMode;
+  
+  modeButtons.forEach(btn => btn.classList.remove("active"));
+
+  if (newMode === "color") {
+    colorModeBtn.classList.add("active");
+  } else if (newMode === "rainbow") {
+    crazyModeBtn.classList.add("active");
+  } else if (newMode === "eraser") {
+    eraserBtn.classList.add("active");
+  }
+
+}
+
+function setCurrentSize(newSize){
+  currentSize = newSize;
+}
+
 
 const container = document.querySelector(".container");
 
@@ -13,72 +37,71 @@ const colorModeBtn = document.querySelector("#colorMode");
 const crazyModeBtn = document.querySelector("#crazyMode");
 const eraserBtn = document.querySelector("#eraser");
 const clearBtn = document.querySelector("#clear");
-const sizeBtn = document.querySelector("#size");
+const sizeSlider = document.querySelector("#sizeSlider");
+const sizeValue = document.querySelector("#sizeValue");
+const sizeValue2 = document.querySelector("#sizeValue2");
 
-let isDrawing = false;
+const modeButtons = document.querySelectorAll("#colorMode, #crazyMode, #eraser");
+
+
+colorPicker.oninput =(e) => setCurrentColor(e.target.value);
+colorModeBtn.onclick =  () => setCurrendMode('color');
+crazyModeBtn.onclick = () => setCurrendMode('rainbow');
+eraserBtn.onclick = () => setCurrendMode('eraser');
+clearBtn.onclick =() => eraseGrid();
+sizeSlider.oninput = (e) => {
+  const newSize = e.target.value;
+
+  sizeValue.textContent = newSize;
+  sizeValue2.textContent = newSize;
+
+  setCurrentSize(newSize);
+  makeGrid(currentSize);
+};
+
+sizeSlider.value = currentSize;
+sizeValue.textContent = currentSize;
+sizeValue2.textContent = currentSize;
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+container.addEventListener("mousedown", paintCell);
+container.addEventListener("mouseover", paintCell);
 
 
 
+function paintCell(e) {
+  console.log(e);console.log(e.target);
 
-// botones
+  if (e.type == 'mouseover' && !mouseDown) return
 
-colorPicker.addEventListener("input", (e) => {
-    currentColor = e.target.value;
-    currentMode = 'COLOR';
-});
-
-colorModeBtn.addEventListener("click", () => {
-  currentColor = DEFAULT_COLOR;
-});
-
-crazyModeBtn.addEventListener('click', () => {
-    currentMode = "RAINBOW";
-});
-
-eraserBtn.addEventListener("click", () => {
-  currentColor = "white";
-});
-
-clearBtn.addEventListener("click", () => {
-  document.querySelectorAll(".cell").forEach(cell => {
-    cell.style.background = "white";
-  });
-});
-
-/// --- MOUSE EVENTS ---
-container.addEventListener("mousedown", (e) => {
-  isDrawing = true;
-  if (e.target.classList.contains("cell")) {
-    paintCell(e.target);
-  }
-});
-
-container.addEventListener("mouseover", (e) => {
-  if (!isDrawing) return;
-  if (e.target.classList.contains("cell")) {
-    paintCell(e.target);
-  }
-});
-
-container.addEventListener("mouseup", () => (isDrawing = false));
-container.addEventListener("mouseleave", () => (isDrawing = false));
-
-function paintCell(cell) {
-  if (currentMode === "RAINBOW") {
+  if (currentMode == "rainbow") {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    cell.style.background = `rgb(${r}, ${g}, ${b})`;
-  } else {
-    cell.style.background = currentColor;
+    e.target.style.background = `rgb(${r}, ${g}, ${b})`;
+  } 
+  else if (currentMode == 'color'){
+    e.target.style.backgroundColor = currentColor;
+  } 
+  else if (currentMode == 'eraser'){
+    e.target.style.backgroundColor = '#ffffff'
   }
 }
 
+function eraseGrid(){
+  container.innerHTML =''
+  makeGrid(currentSize);
+}
 
 
-
-function makeGrid(size = 16) {
+function makeGrid(size) {
   container.innerHTML = "";
+
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
   for (let i = 0; i < size * size; i++) {
     const div = document.createElement("div");
@@ -87,4 +110,21 @@ function makeGrid(size = 16) {
   }
 }
 
-makeGrid(16);
+
+
+
+
+makeGrid(currentSize);
+
+
+const themeToggle = document.getElementById("themeToggle");
+
+themeToggle.addEventListener("click", () => {
+  const current = document.documentElement.getAttribute("data-theme");
+
+  if (current === "dark") {
+    document.documentElement.setAttribute("data-theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
+});
